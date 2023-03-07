c9c0c61630476f2c4b5c3ce5452c934c05d393e6
ArnoldITA20 <arnold.grosberg@voco.ee>
2023-03-07 20:23:53 +0200

    implement filtering for books by status

--- frontend/src/app/components/books-list/books-list.component.ts
+++ frontend/src/app/components/books-list/books-list.component.ts
@@ -16,7 +16,8 @@ export class BooksListComponent implements OnInit {
   books$!: Observable<Page<Book>>;
   dataSource!: MatTableDataSource<Book>;
   searchText: string = '';
-
+  selectedStatus: string = '';
+  filteredBooks: Book[] = [];
 
   constructor(
     private bookService: BookService,
@@ -29,8 +30,34 @@ export class BooksListComponent implements OnInit {
       this.dataSource = new MatTableDataSource(Array.from(page.content));
     });
   }
+
+  applyStatusFilter() {
+    // get all books and filter by the selected status
+    this.bookService.getBooks({}).subscribe((books) => {
+      // if All is selected, apply search filter to all books
+      if (this.selectedStatus === "All") {
+        this.dataSource.data = books.content;
+        this.dataSource.filter = this.searchText.trim().toLowerCase();
+      } else {
+        // filter books by status
+        let filteredBooks = books.content.filter(
+          (book) => book.status === this.selectedStatus
+        );
+        // apply search filter to status filtered books
+        this.dataSource.data = filteredBooks;
+        this.dataSource.filter = this.searchText.trim().toLowerCase();
+      }
+    });
+  }
+
   applyFilter() {
+    // apply status filter if selected
+    if (this.selectedStatus && this.selectedStatus !== "All") {
+      this.applyStatusFilter();
+    } else {
+      // apply search filter
       const filterValue = this.searchText.trim().toLowerCase();
       this.dataSource.filter = filterValue;
     }
+  }
 }

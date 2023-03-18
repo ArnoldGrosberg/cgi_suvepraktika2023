b75690aaa35ec65334b3373de9933ef088067a74
ArnoldITA20 <arnold.grosberg@voco.ee>
2023-03-18 14:36:35 +0200

    implement individual book view

--- frontend/src/app/components/book-detail/book-detail.component.ts
+++ frontend/src/app/components/book-detail/book-detail.component.ts
@@ -4,6 +4,7 @@ import { Book } from '../../models/book';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { map, switchMap } from 'rxjs/operators';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-book-detail',
@@ -12,7 +13,8 @@ import { map, switchMap } from 'rxjs/operators';
 })
 export class BookDetailComponent implements OnInit {
   book$!: Observable<Book>;
-
+  dataSource!: MatTableDataSource<Book>;
+  displayedColumns: string[] = ['id', 'title', 'author', 'genre', 'year', 'added', 'checkOutCount', 'status', 'dueDate', 'comment'];
 
   constructor(
     private route: ActivatedRoute,
@@ -24,6 +26,10 @@ export class BookDetailComponent implements OnInit {
     this.book$ = this.route.params
       .pipe(map(params => params['id']))
       .pipe(switchMap(id => this.bookService.getBook(id)))
+      .pipe(map(book => {
+        this.dataSource = new MatTableDataSource([book]);
+        return book;
+      }));
   }
 
 }
